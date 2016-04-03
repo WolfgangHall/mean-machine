bec28eadc7dba5359655d07f7d99cedc559a1356
WolfgangHall <johnalexander.w.hall@gmail.com>
2016-04-03 18:43:06 -0400

    functioning token auth

--- server.js
+++ server.js
@@ -50,13 +50,12 @@ apiRouter.post('/authenticate', function(req, res) {
       res.json({ success: false, message: 'Authentication failed. Wrong password.' });
     } else {
 
-    // if user is found and password is right
-    // create a token
+    // if user is found and password is right, create a token
     var token = jwt.sign({
       name: user.name,
       username: user.username
     }, superSecret, {
-      expiresInMinutes: 1440 // expires in 24 hours
+      expiresIn: "24h" // expires in 24 hours
     });
 
     // return the information including token as JSON
@@ -65,15 +64,48 @@ apiRouter.post('/authenticate', function(req, res) {
       message: 'Enjoy your token!',
       token: token
     });
+   }
   }
-  }
-  });
  });
+});
 
 apiRouter.use(function(req, res, next){
   console.log('Somebody just came to our app');
 
   next();
+});
+
+// route middleware to verify a token
+apiRouter.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.params.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, superSecret, function(err, decoded) {
+      if (err) {
+        return res.status(403).send({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+
+        next();
+
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an HTTP response of 403 (access forbidden) and an error message
+    return res.status(403).send({ success: false, message: 'No token provided.'
+ });
+
+  }
+  // next() used to be here
 });
 
 apiRouter.get('/', function(req, res){
@@ -152,6 +184,10 @@ apiRouter.route('/users/:user_id')
       res.json({message: 'Successfully deleted'});
     });
   });
+
+apiRouter.get('/me', function(req, res){
+  res.send(req.decoded);
+});
 
  app.use('/api', apiRouter);
 
